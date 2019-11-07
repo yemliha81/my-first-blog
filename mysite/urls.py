4e634159387aefdaf904f71c710b6f50399cf157
yemliha81 <yemlihaddln@hotmail.com>
2019-11-07 10:49:20 +0300

    Django Blog Projesi 3. commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path(r'^advanced_filters/', include('advanced_filters.urls'))
+    path('', include('blog.urls')),
 ]
